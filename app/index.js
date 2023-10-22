fd985ce15f91b26ebb470603424ddf20b19954c1
Fredrik Persson <fp@pol.se>
2023-10-22 11:04:07 +0200

    lag till ändring av datum

--- app/index.js
+++ app/index.js
@@ -18,49 +18,85 @@ export default function index() {
   const [needleData, setNeedleData] = useState("Inget datum registrerat");
   const [sensorData, setSensorData] = useState("Inget datum registrerat");
 
-  const [date, setDate] = useState(new Date(1598051730000));
+  const [date, setDate] = useState(new Date());
   const [mode, setMode] = useState("date");
-  const [show, setShow] = useState(false);
+  const [showInsulinDateTime, setShowInsulinDateTime] = useState(false);
+
+  //const [date, setDate] = useState(new Date(1598051730000));
+  const [showSensorDateTime, setShowSensorDateTime] = useState(false);
+
+  //const [date, setDate] = useState(new Date(1598051730000));
+  const [showNeelDateTime, setShowNeelDateTime] = useState(false);
+
+  function changeTimeToNoon(inputStr) {
+    // Använd en regex för att matcha klockslag (HH:MM format)
+    let regex = /(\d{1,2}:\d{2})/;
+
+    // Ersätt det befintliga klockslaget med "12:00"
+    return inputStr.replace(regex, "12:00");
+  }
 
   const onChangeInsulin = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setShow(false);
-    setDate(currentDate);
+    setShowInsulinDateTime(false);
+    const _insulinDate = addThreeDays(selectedDate);
+
+    let dateTimeStr = _insulinDate;
+    let updatedDateTimeStr = changeTimeToNoon(dateTimeStr);
+    setInsulinData(updatedDateTimeStr);
   };
 
   const onChangeSensor = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setShow(false);
-    setDate(currentDate);
+    setShowSensorDateTime(false);
+    const _sensorDate = addTenDays(selectedDate);
+
+    let dateTimeStr = _sensorDate;
+    let updatedDateTimeStr = changeTimeToNoon(dateTimeStr);
+
+    setSensorData(updatedDateTimeStr);
   };
 
   const onChangeNeel = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setShow(false);
-    setDate(currentDate);
+    setShowNeelDateTime(false);
+    const _neelDate = addThreeDays(selectedDate);
+
+    let dateTimeStr = _neelDate;
+    let updatedDateTimeStr = changeTimeToNoon(dateTimeStr);
+
+    setNeedleData(updatedDateTimeStr);
   };
 
-  const showMode = (currentMode) => {
-    setShow(true);
+  const showMode = (currentMode, index) => {
+    console.log("index", index);
+    if (index === 0) {
+      setShowInsulinDateTime(true);
+    }
+    if (index === 1) {
+      setShowSensorDateTime(true);
+    }
+    if (index === 2) {
+      setShowNeelDateTime(true);
+    }
+
+    //setShow(true);
     setMode(currentMode);
   };
 
-  const showDatepicker = () => {
-    showMode("date");
-  };
-
-  const showTimepicker = () => {
-    showMode("time");
+  const showDatepicker = (index) => {
+    showMode("date", index);
   };
 
   moment.locale("sv");
 
-  const addThreeDays = () => {
-    return moment().add(3, "days").format("dddd, Do MMMM , HH:mm");
+  const addThreeDays = (date) => {
+    //if (date != null) {
+    const newDate = moment(date).add(3, "days").format("dddd, Do MMMM , HH:mm");
+    return moment(date).add(3, "days").format("dddd, Do MMMM , HH:mm");
+    //}
+    //return moment().add(3, "days").format("dddd, Do MMMM , HH:mm");
   };
 
-  const addTenDays = () => {
-    return moment().add(10, "days").format("dddd, Do MMMM , HH:mm");
+  const addTenDays = (date) => {
+    return moment(date).add(10, "days").format("dddd, Do MMMM , HH:mm");
   };
 
   const saveInsulinData = async (insulinDatum) => {
@@ -128,31 +164,73 @@ export default function index() {
   const onPressInsulin = () => {
     const _insulindate = addThreeDays();
     console.log(_insulindate);
+    setShowInsulinDateTime(false);
     setInsulinData(_insulindate);
     saveInsulinData(_insulindate);
   };
   const onPressNeedle = () => {
     const _needledate = addThreeDays();
+    setShowNeelDateTime(false);
     setNeedleData(_needledate);
     saveNeedleData(_needledate);
   };
   const onPressSensor = () => {
     const _sensordate = addTenDays();
+    setShowSensorDateTime(false);
     setSensorData(_sensordate);
     saveSensorData(_sensordate);
   };
 
   return (
     <SafeAreaView className="bg-fuchsia-200  flex-1 pt-2 pr-2 pl-2">
-      <View>
-        {show && (
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={date}
-            mode={mode}
-            is24Hour={true}
-            onChange={onChangeInsulin}
-          />
+      <View className="flex">
+        {showInsulinDateTime && (
+          <View className="m-2 items-center justify-center bg-[#d9a3ff] rounded-xl">
+            <View>
+              <View>
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode={mode}
+                  is24Hour={true}
+                  onChange={onChangeInsulin}
+                  display="default"
+                />
+              </View>
+            </View>
+          </View>
+        )}
+        {showSensorDateTime && (
+          <View className="m-2 items-center justify-center bg-[#d9a3ff] rounded-xl">
+            <View>
+              <View>
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode={mode}
+                  is24Hour={true}
+                  onChange={onChangeSensor}
+                  display="default"
+                />
+              </View>
+            </View>
+          </View>
+        )}
+        {showNeelDateTime && (
+          <View className="m-2 items-center justify-center bg-[#d9a3ff] rounded-xl">
+            <View>
+              <View>
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode={mode}
+                  is24Hour={true}
+                  onChange={onChangeNeel}
+                  display="default"
+                />
+              </View>
+            </View>
+          </View>
         )}
       </View>
       <View className="flex-1 justify-between">
@@ -168,8 +246,8 @@ export default function index() {
             </View>
             <View>
               <TouchableOpacity
-                className="bg-fuchsia-300 rounded-full h-20 w-20  items-center justify-center"
-                onPress={() => showDatepicker()}
+                className="bg-[#6473d0] rounded-full h-20 w-20  items-center justify-center"
+                onPress={() => showDatepicker(0)}
               >
                 <Text className="text-white text-lg font-bold">Justera</Text>
               </TouchableOpacity>
@@ -183,37 +261,64 @@ export default function index() {
         </View>
 
         <View className="m-2 flex-1 items-center justify-center  bg-[#504099] rounded-xl">
-          <TouchableOpacity
-            className=" bg-[#FE7BE5] rounded-full h-20 w-20  items-center justify-center"
-            onPress={() => onPressSensor(moment.now())}
-          >
-            <Text className="text-white text-lg font-bold">Sensor</Text>
-          </TouchableOpacity>
-          <Text className="text-center text-white mt-2 font-bold text-xl pt-2">
-            {sensorData}
-          </Text>
+          <View className="mt-4 flex flex-row justify-evenly w-full">
+            <View>
+              <TouchableOpacity
+                className=" bg-[#FE7BE5] rounded-full h-20 w-20  items-center justify-center"
+                onPress={() => onPressSensor(moment.now())}
+              >
+                <Text className="text-white text-lg font-bold">Sensor</Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity
+                className=" bg-[#7d67e0] rounded-full h-20 w-20  items-center justify-center"
+                onPress={() => showDatepicker(1)}
+              >
+                <Text className="text-white text-lg font-bold">Justera</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View className="m-2">
+            <Text className="text-center text-white mt-2 font-bold text-xl pt-2">
+              {sensorData}
+            </Text>
+          </View>
         </View>
 
         <View className="m-2 flex-1 items-center justify-center bg-[#974EC3] rounded-xl">
-          <TouchableOpacity
-            className="w-20 h-20 bg-[#FE7BE5] rounded-full items-center justify-center"
-            onPress={() => onPressNeedle(moment.now())}
-          >
-            <Text className="text-white text-lg font-bold">Nål</Text>
-          </TouchableOpacity>
-          <Text className="text-center text-white mt-2 font-bold text-xl pt-2">
-            {needleData}
-          </Text>
+          <View className="mt-4 flex flex-row justify-evenly w-full">
+            <View>
+              <TouchableOpacity
+                className=" bg-[#FE7BE5] rounded-full h-20 w-20  items-center justify-center"
+                onPress={() => onPressNeedle(moment.now())}
+              >
+                <Text className="text-white text-lg font-bold">Nål</Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity
+                className="bg-[#b961ef] rounded-full h-20 w-20  items-center justify-center"
+                onPress={() => showDatepicker(2)}
+              >
+                <Text className="text-white text-lg font-bold">Justera</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View className="m-2">
+            <Text className="text-center text-white mt-2 font-bold text-xl pt-2">
+              {needleData}
+            </Text>
+          </View>
         </View>
 
         <View className="m-2 p-2  items-center justify-center ">
           <TouchableOpacity
-            className="w-16 h-16 bg-[#FE7BE5] rounded-full items-center justify-center"
+            className="w-16 h-16 bg-[#c32da7] rounded-full items-center justify-center"
             onPress={() => removeFew()}
           >
             <Text className="text-white text-sm font-bold">Rensa</Text>
           </TouchableOpacity>
-          <Link href="/home">About</Link>
         </View>
       </View>
       <StatusBar style="light" />
